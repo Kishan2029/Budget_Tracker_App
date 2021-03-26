71fd0acab21be0717df5ddd517a3146d4f3ef042
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-26 10:40:25 +0530

    Header is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+
 import './App.css';
 
 const App = () =>{
-    return <p>Hello</p>
+  return(
+    <div className='container'>
+        <h1 className='mt-3'>My Budget Planner</h1>
+        <div className='row mt-3'>
+            <div className='col-sm'>
+              <Budget/>
+            </div>
+            <div className='col-sm'>
+              <Remaining/>
+            </div>
+            <div className='col-sm'>
+              <ExpenseTotal/>
+            </div>
+        </div>
+        
+
+    </div>
+
+  )
+    
 }
 
 export default App;
