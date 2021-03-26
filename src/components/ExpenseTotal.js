fdef228c3742c333595bbfb3498e2efdaa3477df
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-26 10:34:54 +0530

    init project

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseTotal = () =>{
+    return(
+        <div className='alert alert-secondary'>
+            <span>Spent so far: $1000</span>
+        </div>
+    )
+}
+
+export default ExpenseTotal;
